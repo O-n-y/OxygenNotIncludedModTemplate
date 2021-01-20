e0236ba784c452d99c284c9808aaa686caa926b2
O-n-y <O-n-y@users.noreply.github.com>
2021-01-20 13:01:22 +0300

    Added ManualGeneratorAdvanced project

---
+++ ModsGuide/ManualGeneratorAdvanced/Loader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using Harmony;
+using UnityEngine;
+
+namespace OxygenNotIncluded.Mods.ManualGeneratorAdvanced
+{
+	public class Loader : LoaderBase
+	{
+		public static AssemblyName AssemblyName => Assembly.GetExecutingAssembly().GetName();
+		public static Version Version => AssemblyName.Version;
+		public static string Name => AssemblyName.Name;
+
+
+		public static void OnLoad()
+		{
+
+			// Called before any other mod functions (including patches), when Mod is loaded by the Game
+			Console.WriteLine($"Mod <{Name}> loaded: {Version}");
+		}
+	}
+}
---
+++ ModsGuide/ManualGeneratorAdvanced/Patches/ManualGeneratorConfig_Patch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Epic.OnlineServices.Stats;
+using Harmony;
+
+namespace ManualGeneratorAdvanced.Patches
+{
+	[HarmonyPatch(typeof(ManualGeneratorConfig), "CreateBuildingDef")]
+	class ManualGeneratorConfig_CreateBuildingDef
+	{
+		public static void Postfix(BuildingDef __result)
+		{
+			__result.GeneratorWattageRating = 600f;
+		}
+	}
+}
