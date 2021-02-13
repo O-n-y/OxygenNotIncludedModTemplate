f5ed29a40df4279700bb0f17a535ada127bf3c4d
O-n-y <O-n-y@users.noreply.github.com>
2021-02-13 17:31:13 +0300

    update for SharedLib

--- ModsGuide/SharedLib/Loader.cs
+++ ModsGuide/SharedLib/Loader.cs
@@ -8,13 +8,14 @@ namespace OxygenNotIncluded.Mods.SharedLib
 		public static AssemblyName AssemblyName => Assembly.GetExecutingAssembly().GetName();
 		public static Version Version => AssemblyName.Version;
 		public static string Name => AssemblyName.Name;
-		
-		
+
 		public static void OnLoad()
 		{
-
+			
 			// Called before any other mod functions (including patches), when Mod is loaded by the Game
+			
 			Console.WriteLine($"Mod <{Name}> loaded: {Version}");
+		
 		}
 	}
 }
